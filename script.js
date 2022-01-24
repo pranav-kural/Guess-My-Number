3754889d5bb9699204cd21ab5026408babf1b749
pranav-kural <pranavkural042@gmail.com>
2022-01-24 18:02:55 +0530

    implemented the score functionality

--- script.js
+++ script.js
@@ -2,9 +2,20 @@
 
 // define the secret number
 let secretNum = Math.trunc(Math.random()*20) + 1;
+// define score
+let score = 20; // initially 20 (max)
 
 // TEST
 document.querySelector('.number').textContent = secretNum;
+
+// decrement score by 1 and update DOM
+function decrementScore() {
+    // decrement the score
+    score--;
+    // show updated score
+    document.querySelector('.score').textContent = score;
+}
+
 
 // add event listener for the "check" button
 document.querySelector('.check').addEventListener( 'click', (e) => {
@@ -12,16 +23,27 @@ document.querySelector('.check').addEventListener( 'click', (e) => {
     const GUESS = Number(document.querySelector('.guess').value);
     // TEST
     console.log("Guess: " + GUESS);
-    // check guess
-    if (!GUESS) {
-        // if no guess made, or value 0
-        document.querySelector('.message').textContent = "😿 No valid number guessed!";
-    } else if (GUESS == secretNum) {
-        document.querySelector('.message').textContent = "🥳 Correct Number!";
-    } else if (GUESS > secretNum) {
-        document.querySelector('.message').textContent = "📈 Too high!";
-    } else if (GUESS < secretNum) {
-        document.querySelector('.message').textContent = "📉 Too low!";
+    // check if user hasn't already lost the game
+    if (score > 1) {
+        // check guess
+        if (!GUESS) {
+            // if no guess made, or value 0
+            document.querySelector('.message').textContent = "😵 No valid number guessed!";
+        } else if (GUESS == secretNum) {
+            document.querySelector('.message').textContent = "🥳 Correct Number!";
+        } else if (GUESS > secretNum) {
+            document.querySelector('.message').textContent = "📈 Too high!";
+            // decrement the score and display new score
+            decrementScore();
+        } else if (GUESS < secretNum) {
+            document.querySelector('.message').textContent = "📉 Too low!";
+            // decrement the score and display new score
+            decrementScore();
+        }
+    } else {
+        // set score to zero
+        decrementScore();
+        document.querySelector('.message').textContent = "😿 You lost the game!";
     }
 
 });
